926b4d05489acc55a893d5149c638d16f3f72efb
veselinovic93 <veselinovic93@hotmail.com>
2024-09-16 23:52:49 +0200

    chore: setupa and install husky

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-
+import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
     ignores: ['node_modules', 'build'],
+    languageOptions: { globals: globals.browser },
 });
